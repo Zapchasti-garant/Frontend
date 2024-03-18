ddb90efba9367dd32c815a8ece30ca273efb6410
Вячеслав Воробьев <slavavorobey@vk.com>
2024-03-19 00:00:21 +0700

    init

---
+++ src/components/AsideBar.vue
@@ -0,0 +1,21 @@
+<template>
+	<aside>
+		<nav>
+			<ul class="list-reset">
+				<li>
+					<router-link to="/">Главная</router-link>
+				</li>
+				<li>
+					<router-link to="/catalog">Каталог</router-link>
+				</li>
+				<li>
+					<router-link class="" to="/">Корзина</router-link>
+				</li>
+			</ul>
+		</nav>
+	</aside>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
