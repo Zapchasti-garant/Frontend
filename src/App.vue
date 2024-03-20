2ebf17966158b9d8f71fd8c2a58bb5ba169bf547
Вячеслав Воробьев <slavavorobey@vk.com>
2024-03-20 23:45:58 +0700

    [add container and routerview]

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
   <Header />
   <Navigation />
+  <Carousel v-if="showCarousel" />
+  <Background v-if="!showCarousel" />
   <main class="main">
-    <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
-    </router-view>
+    <div class="container">
+      <div class="main__wrapper">
+        <BaseLeftPanel />
+        <div>
+          <router-view v-slot="{ Component }">
+            <transition name="fade" mode="out-in">
+              <component :is="Component" />
+            </transition>
+          </router-view>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
 import Header from "@/components/Base/BaseHeader.vue";
+import BaseLeftPanel from "@/components/Base/BaseLeftPanel.vue";
 import Navigation from "@/components/Base/BaseNavigation.vue";
-import { Transition } from "vue";
-import BaseLeftPanel from "./components/Base/BaseLeftPanel.vue";
+import { Transition, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import Background from "./ui/Background.vue";
+import Carousel from "./ui/Carousel.vue";
+
+const showCarousel = ref(true);
+const route = useRoute();
+watch(
+  () => route.path,
+  (newVal) => {
+    if (newVal === "/") {
+      showCarousel.value = true;
+    } else {
+      showCarousel.value = false;
+    }
+  }
+);
 </script>
 
 <style>
@@ -29,5 +54,9 @@ import BaseLeftPanel from "./components/Base/BaseLeftPanel.vue";
 }
 .main {
   margin: 0 auto;
+  padding-top: 25px;
+}
+.main__wrapper {
+  display: flex;
 }
 </style>
