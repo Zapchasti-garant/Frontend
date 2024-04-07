91330821fe9cde87cfcb0ba6996ed471af905952
Вячеслав Воробьев <slavavorobey@vk.com>
2024-04-07 20:09:42 +0700

    add conf

--- scripts/config.js
+++ scripts/config.js
@@ -6,6 +6,9 @@ const __dirname = dirname(__filename);
 
 const source = join(__dirname, '..', '.htaccess');
 const destination = join(__dirname, '..', 'public', '.htaccess');
+const nginx = join(__dirname, '..', 'nginx.conf');
+const target = join(__dirname, '..', 'public', 'nginx.conf');
 
 import { copyFileSync } from 'fs';
 copyFileSync(source, destination);
+copyFileSync(nginx, target);
